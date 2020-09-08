34e4974621dac724b9d5278fe9bf5579cfbb20ad
cheese stick <61661631+longhike@users.noreply.github.com>
2020-09-08 16:12:33 -0400

    modals set up, need to add local storage stuff for high scores

--- script.js
+++ script.js
@@ -1,177 +1,210 @@
-// set questions, correct answer, and answer options
-var questions = [
-  {
-    question: 'On what planet was Anakin Skywalker born?',
-    answer: 'Tatooine',
-    options: ['Tatooine', 'Dagoba', 'Coruscant', 'Naboo']
-  },
-  {
-    question: 'What is the symbiotic microbe that allows Jedi to feel the Force?',
-    answer: 'midiclorians',
-    options: ['red blood cells', 'white blood cells', 'midiclorians', 'grey matter']
-  },
-  {
-    question: 'Who is Darth Vader?',
-    answer: 'Anakin Skywalker',
-    options: ['Luke Skywalker', 'Anakin Skywalker', 'Yoda', 'C3PO']
-  }
-]
+// VARIABLE ASSIGNMENTS
+    // grabs highScore div
+    const highScore = document.getElementById('viewHighScores')
 
-console.log(questions)
+    // grabs whole question card
+    const questionCard = document.getElementById('question-card')
+    
+    // grabs section for question text
+    const questionContent = document.getElementById('question-container')
+    console.log(questionContent);
+    
+    // grabs start button
+    const startBtn = document.getElementById('startBtn')
+    console.log(startBtn);
+    
+    // grab button holder div
+    const buttonHolder = document.getElementsByClassName('controls')[0]
+    console.log(buttonHolder);
+    
+    // grab answer button by class
+    const buttonAnswer = document.getElementsByClassName('answer')
 
-// results variable
-var results = {
-  correct: 0,
-  false: 0
-}
+    // grab modal by class
+    const modal = document.getElementsByClassName('modal')[0]
 
-var countdown;
-var timer = 10
-// variable for question index
-var qIndex = 0
+    //grabs close modal by class
+    const closeModal = document.getElementsByClassName('_close')[0]
+    console.log(closeModal);
 
-var highScore = document.getElementById('viewHighScores')
+    // grabs info card by id
+    const infoCard = document.getElementById('info-card')
+    console.log(infoCard);
 
-console.log(results)
+    // grabs final score div
+    const finalScore = document.getElementById('final-score')
 
-// variable 'questionContainer' to clear the 
-var questionContainer = document.getElementById('questionContainer')
-// 'hideStart' to remove start button
-var startBtn = document.getElementById('startBtn')
-// timer selector
-var timerEl = document.getElementsByClassName('timer-sam')[0]
+    //grabs the play again button
+    const playAgain = document.getElementById('play-again')
+    
+    // set timer variables:
+    let countdown
+    let timer = 30
+    let timerEl = document.getElementsByClassName('timer-sam')[0]
+
+    // set question and score variables
+    let qIndex = 0
+
+    let score = {
+        correct: 0,
+        incorrect: 0
+    }
 
 
 
-// ######################### FUNCTIONS ######################### 
+// QUESTION SETUP
+    const questions = [
+        {
+            question: "What is the name of Han Solo's spaceship?",
+            answer: 'Millenium Falcon',
+            options: ['Millenium Falcon', 'Centennial Pigeon', 'Lunar Moth', 'Kessel Run']
+        },
+        {
+            question: 'On what planet was Anakin Skywalker born?',
+            answer: 'Tatooine',
+            options: ['Tatooine', 'Dagoba', 'Coruscant', 'Naboo']
+        },
+        {
+            question: 'What is the symbiotic microbe that allows Jedi to feel the Force?',
+            answer: 'midiclorians',
+            options: ['red blood cells', 'white blood cells', 'midiclorians', 'grey matter']
+        },
+        {
+            question: 'Who is Darth Vader?',
+            answer: 'Anakin Skywalker',
+            options: ['Luke Skywalker', 'Anakin Skywalker', 'Yoda', 'C3PO']
+        },
+        {
+            question: "What color is Mace Windu's lightsaber?",
+            answer: 'purple',
+            options: ['red', 'purple', 'green', 'blue']
+        }
+    ]
 
-// start the quiz, run question function, timer function
-startBtn.addEventListener('click', function (e) {
-  // e.preventDefault() -- doesn't exist as not contained in <form></form>
-  // removes start button
-  startBtn.style.display = "none"
-  runTimer()
-  nextQuestion()
+
+// LISTENERS AND FUNCTIONS
+
+
+startBtn.addEventListener('click', () => {
+    startBtn.style.display = 'none'
+    runTimer()
+    changeShit()
 })
 
-
-// question function (add correct answer, wrong answer conditional)
-function nextQuestion() {
-  // set nextQuestion function to the current index 
-  var currentQuestion = questions[qIndex];
-
-  // create the template for the questions
-  // add break
-  var addBreak = document.createElement('br')
-  // add text of first question
-  var setQuestion = document.createElement('p')
-  // add option div
-  var addFirstOptions = document.createElement('div')
-  // add buttons
-  var addFirstButton = document.createElement('button')
-  var addSecondButton = document.createElement('button')
-  var addThirdButton = document.createElement('button')
-  var addFourthButton = document.createElement('button')
-  //set the question text
-  setQuestion.setAttribute('style', 'font-size: larger')
-  setQuestion.innerText = currentQuestion.question
-  // set the grid for the option buttons
-  addFirstOptions.setAttribute('id', 'answer-buttons')
-  addFirstOptions.setAttribute('class', 'btn-grid')
-
-  // set buttons - first button
-  addFirstButton.setAttribute('id', 'opt1')
-  addFirstButton.setAttribute('class', 'btn answer')
-  addFirstButton.innerText = currentQuestion.options[0]
-  addFirstButton.addEventListener('click', checkAnswer)
-  // addFirstButton.onclick = handleClick
-  // set buttons - second button
-  addSecondButton.setAttribute('id', 'opt2')
-  addSecondButton.setAttribute('class', 'btn answer')
-  addSecondButton.innerText = currentQuestion.options[1]
-  addSecondButton.addEventListener('click', checkAnswer)
-  // addSecondButton.onclick = handleClick
-  // set buttons - third button
-  addThirdButton.setAttribute('id', 'opt3')
-  addThirdButton.setAttribute('class', 'btn answer')
-  addThirdButton.innerText = currentQuestion.options[2]
-  addThirdButton.addEventListener('click', checkAnswer)
-  // addThirdButton.onclick = handleClick
-  // set buttons - fourth button
-  addFourthButton.setAttribute('id', 'opt4')
-  addFourthButton.setAttribute('class', 'btn answer')
-  addFourthButton.innerText = currentQuestion.options[3]
-  addFourthButton.addEventListener('click', checkAnswer)
-  // addFourthButton.onclick = handleClick
-  // clears whole container
-  questionContainer.innerHTML = ''
-
-  // add break
-  questionContainer.appendChild(addBreak)
-  //create the question text
-  questionContainer.appendChild(setQuestion)
-  // create buttons - first button
-  addFirstOptions.appendChild(addFirstButton)
-  // create buttons - second button
-  addFirstOptions.appendChild(addSecondButton)
-  // create buttons - third button
-  addFirstOptions.appendChild(addThirdButton)
-  // create buttons - fourth button
-  addFirstOptions.appendChild(addFourthButton)
-  // create the grid for the option buttons
-  questionContainer.appendChild(addFirstOptions)
-}
-
-// timer function
-function runTimer() {
-  // clear the timer first, so that if accidentally click 'start' again, resets
-  clearInterval(countdown);
-
-  countdown = setInterval(function () {
-    timer--
-    timerEl.textContent = timer
-
-    if (timer <= 0) {
-      endGame('Oops! Out of time! ')
+function changeShit () {
+    
+    if (questions[qIndex] === undefined) {
+        endGame('Game over! ')
+        
+    } else {
+        questionContent.innerText = ''
+        questionContent.innerText = questions[qIndex].question
+        removeButtons()
+        addButtons()
+        getAnswer()
+        qIndex++    
     }
-  }, 1000)
 }
 
-// highscores function (to show high scores div)
-
-function showHighScore() {
-  questionContainer.innerHTML = ''
-
-  // code here to show score within question-container 
-
+function addButtons () {
+    
+    for (i = 0; i < questions[qIndex].options.length; i++) {
+        let arrayButtons =  document.createElement('div')
+        arrayButtons.setAttribute('class', 'btn btn-info answer')
+        console.log(arrayButtons);
+        arrayButtons.innerText = questions[qIndex].options[i]
+        buttonHolder.appendChild(arrayButtons)
+    }
 }
 
-
-function checkAnswer() {
-  console.log(this.innerText === questions[qIndex].answer)
-
-  if (this.innerText === questions[qIndex].answer) {
-    results.correct++
-  }
-  else {
-    results.false++
-  }
-
-  qIndex++;
-
-  if (qIndex < questions.length) {
-    nextQuestion()
-  } else {
-    endGame('Game Over! ')
-  }
+function removeButtons () {
+    buttonHolder.innerHTML = ''
 }
+
+function getAnswer () {
+    let answerButtons = document.getElementsByClassName('answer')
+    console.log(answerButtons);
+    for (i = 0; i < answerButtons.length; i++) {
+        answerButtons[i].addEventListener('click', function () {
+            console.log(this.innerText);
+            console.log(questions[qIndex - 1].answer);
+
+        if (this.innerText === questions[qIndex - 1].answer) {
+            console.log(this.innerText);
+            console.log(questions[qIndex - 1].answer);
+            alert('Nice! Got that one.')
+            score.correct++
+            console.log(score);
+            changeShit()
+        } else {
+            console.log(this.innerText);
+            console.log(questions[qIndex - 1].answer);
+            alert('You fool! Do better on the next one.')
+            score.incorrect++
+            console.log(score);
+            changeShit()
+        }
+        })
+    } 
+    
+}
+
+function runTimer() {
+    // clear the timer first, so that if accidentally click 'start' again, resets
+    clearInterval(countdown);
+  
+    countdown = setInterval(function () {
+      timer--
+      timerEl.textContent = timer
+  
+      if (timer <= 0) {
+        endGame('Oops! Out of time! ')
+      }
+    }, 1000)
+  }
 
 function endGame(msg) {
-  clearInterval(countdown)
-  alert(`${msg} Here are your results: Correct: ${results.correct}; False: ${results.false}`)
-  location.reload()
+    clearInterval(countdown)
+    questionCard.style.display = 'none';
+    infoCard.style.display = 'block';
+
+        finalScore.innerHTML =
+        `<h4>${msg}</h4>
+        <h5>Here are your results:</h5> 
+        <p>Correct: ${score.correct}</p>
+        <p>Incorrect: ${score.incorrect}</p>
+        <p>Missed: ${questions.length - (score.correct + score.incorrect)}</p>
+        `;
 }
 
-highScore.addEventListener('click', () => {
-  alert('Not yet... to come!')
+playAgain.addEventListener('click', (e) => {
+    e.preventDefault()
+    location.reload()
 })
+
+highScore.addEventListener('click', (e) => {
+        e.preventDefault()
+        modal.style.display = 'block'
+    }
+)
+
+closeModal.addEventListener('mouseover', (e) => {
+    e.preventDefault()
+    closeModal.style.cursor = 'pointer'
+})
+
+closeModal.addEventListener('mousedown', (e) => {
+        e.preventDefault()
+        console.log('clicked!');
+        closeModal.style.color = 'black'
+    }
+)
+
+closeModal.addEventListener('mouseup', (e) => {
+    e.preventDefault()
+    console.log('clicked!');
+    modal.style.display = 'none'
+    closeModal.style.color = '#aaa'
+}
+)
+
