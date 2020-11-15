9383eb6f239fb5dc166c305fd5695553b619ac09
cheese stick <61661631+longhike@users.noreply.github.com>
2020-11-15 12:24:20 -0500

    cleaned up

--- public/js/script.js
+++ public/js/script.js
@@ -1,57 +1,36 @@
 // VARIABLE ASSIGNMENTS
     // grabs highScore div
     const highScore = document.getElementById('viewHighScores')
-
     // grabs whole question card
     const questionCard = document.getElementById('question-card')
-    
     // grabs section for question text
     const questionContent = document.getElementById('question-container')
-    console.log(questionContent);
-    
     // grabs start button
     const startBtn = document.getElementById('startBtn')
-    console.log(startBtn);
-    
     // grab button holder div
     const buttonHolder = document.getElementsByClassName('controls')[0]
-    console.log(buttonHolder);
-    
     // grab answer button by class
     const buttonAnswer = document.getElementsByClassName('answer')
-
     // grab modal by class
     const modal = document.getElementsByClassName('modal')[0]
-
     //grabs close modal by class
     const closeModal = document.getElementsByClassName('_close')[0]
-    console.log(closeModal);
-
     // grabs info card by id
     const infoCard = document.getElementById('info-card')
-    console.log(infoCard);
-
     // grabs final score div
     const finalScore = document.getElementById('final-score')
-
     //grabs the play again button
     const playAgain = document.getElementById('play-again')
-    
     // set timer variables:
     let countdown
     let timer = 30
     let timerEl = document.getElementsByClassName('timer-div')[0]
-
     // set question and score variables
     let qIndex = 0
-
     let score = {
         correct: 0,
         incorrect: 0
     }
-
-
-
 // QUESTION SETUP
     const questions = [
         {
@@ -80,11 +59,7 @@
             options: ['red', 'purple', 'green', 'blue']
         }
     ]
-
-
 // LISTENERS AND FUNCTIONS
-
-
 startBtn.addEventListener('click', () => {
     startBtn.style.display = 'none'
     runTimer()
@@ -111,39 +86,26 @@ function addButtons () {
     for (i = 0; i < questions[qIndex].options.length; i++) {
         let arrayButtons =  document.createElement('div')
         arrayButtons.setAttribute('class', 'btn btn-info answer')
-        console.log(arrayButtons);
         arrayButtons.innerText = questions[qIndex].options[i]
         buttonHolder.appendChild(arrayButtons)
     }
 }
 
 function removeButtons () {
-    buttonHolder.innerHTML = ''
+    buttonHolder.textContent = ''
 }
 
 function getAnswer () {
     let answerButtons = document.getElementsByClassName('answer')
-    console.log(answerButtons);
     for (i = 0; i < answerButtons.length; i++) {
         answerButtons[i].addEventListener('click', function () {
-            console.log(this.innerText);
-            console.log(questions[qIndex - 1].answer);
-
         if (this.innerText === questions[qIndex - 1].answer) {
-            console.log(this.innerText);
-            console.log(questions[qIndex - 1].answer);
             this.setAttribute('style','background-color: green;')
-            // alert('Nice! Got that one.')
             score.correct++
-            console.log(score);
             setTimeout(changeStuff, 1000)
         } else {
-            console.log(this.innerText);
-            console.log(questions[qIndex - 1].answer);
-            // alert('You fool! Do better on the next one.')
             this.setAttribute('style','background-color: red;')
             score.incorrect++
-            console.log(score);
             setTimeout(changeStuff, 1000)
         }
         })
@@ -197,14 +159,12 @@ closeModal.addEventListener('mouseover', (e) => {
 
 closeModal.addEventListener('mousedown', (e) => {
         e.preventDefault()
-        console.log('clicked!');
         closeModal.style.color = 'black'
     }
 )
 
 closeModal.addEventListener('mouseup', (e) => {
     e.preventDefault()
-    console.log('clicked!');
     modal.style.display = 'none'
     closeModal.style.color = '#aaa'
 }
